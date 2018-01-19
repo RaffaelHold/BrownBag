437b7e7428ba57ef02c3ff0f7d851b0f43decfb6
Raffael Hold <raffael.hold@acando.de>
2018-01-19 11:39:22 +0100

    Initial commit

---
+++ GitExecuter/Controllers/GitController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitExecuter.Controllers
+{
+    [Route("api/[controller]")]
+    public class GitController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public void Get(/*string command, string argument*/)
+        {
+            var executor = new GitExecutor();
+            executor.Execute("", "");
+        }
+    }
+}
---
+++ GitExecuter/GitExecuter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitExecuter
+{
+    public class GitExecutor
+    {
+        public void Execute(string command, string param)
+        {
+            Process.Start("git", "commit -m \"Initial commit\"");
+        }
+    }
+}
